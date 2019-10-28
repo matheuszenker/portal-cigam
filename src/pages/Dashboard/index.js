fa7f9bacdf29ad62d0b62981e00574d029ef6d17
Matheus Zenker <matheus.g.zenker@gmail.com>
2019-10-28 14:55:34 -0300

    Initial

---
+++ src/pages/Dashboard/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { Title } from './styles';
+
+function Dashboard() {
+  return (
+    <Title error>
+      Dashboard
+      <small>Teste</small>
+    </Title>
+  );
+}
+
+export default Dashboard;
